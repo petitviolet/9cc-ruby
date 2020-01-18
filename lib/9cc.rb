7dc058d4b4ade7c2bd438681d3c4e81d1694fcb4
petitviolet <violethero0820@gmail.com>
2020-01-18 23:13:50 +0900

    Implemented Generator

--- lib/9cc.rb
+++ lib/9cc.rb
@@ -8,6 +8,47 @@ class Program
     @user_input = user_input
   end
 
+  class Generator
+    def self.run(nodes, assemblies = [])
+      case Array(nodes)
+      in []
+        return []
+      in [Node::Num[value], *rest]
+        assemblies << "  push #{value}"
+        run(rest, assemblies)
+      else
+        assemblies << "  pop rdi"
+        assemblies << "  pop rax"
+
+        case Array(nodes)
+        in [Node::Add[left, right], *rest]
+          run(left, assemblies)
+          run(right, assemblies)
+          assemblies << "  add rax, rdi"
+          run(rest, assemblies)
+        in [Node::Sub[left, right], *rest]
+          run(left, assemblies)
+          run(right, assemblies)
+          assemblies << "  sub rax, rdi"
+          run(rest, assemblies)
+        in [Node::Mul[left, right], *rest]
+          run(left, assemblies)
+          run(right, assemblies)
+          assemblies << "  imul rax, rdi"
+          run(rest, assemblies)
+        in [Node::Div[left, right], *rest]
+          run(left, assemblies)
+          run(right, assemblies)
+          assemblies << "  cqo"
+          assemblies << "  idiv rdi"
+           run(rest, assemblies)
+        end
+      end
+
+      assemblies << "  push rax"
+    end
+  end
+
   def run
     tokens = Token.tokenize(@user_input)
     pp tokens
@@ -19,7 +60,7 @@ class Program
     outputs << ".intel_syntax noprefix"
     outputs << ".global main"
     outputs << "main:"
-
+    #Generator.run(nodes, outputs)
     case tokens
     in [Token::Num => num, *rest]
       # The first token must be a number
@@ -44,6 +85,7 @@ class Program
 
     puts outputs.join("\n")
   end
+
 end
 
 Program.new(ARGV.first).run
