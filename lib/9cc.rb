17ef957d09b67c48bbc8ccd18768003e79236eea
petitviolet <violethero0820@gmail.com>
2020-01-20 23:44:51 +0900

    Implemented generator for >=, >, <=, <, ==, !=

--- lib/9cc.rb
+++ lib/9cc.rb
@@ -12,44 +12,64 @@ class Program
 
   class Generator
     def self.run(nodes, assemblies = [])
-      case Array(nodes)
+      case Array(nodes).flatten
       in []
         return []
       in [Node::Num[value], *rest]
         assemblies << "  push #{value}"
         return run(rest, assemblies)
       else
+        run_left_and_right = ->(left, right, rest, &block) do
+          run(left, assemblies)
+          run(right, assemblies)
+          assemblies << "  pop rdi"
+          assemblies << "  pop rax"
+          block.call
+          run(rest, assemblies)
+        end
 
-        case Array(nodes)
+        case Array(nodes).flatten
+        in [Node::Eq[left, right], *rest]
+          run_left_and_right.call(left, right, rest) do
+            assemblies << "  cmp rax, rdi"
+            assemblies << "  sete al"
+            assemblies << "  movzb rax, al"
+          end
+        in [Node::Neq[left, right], *rest]
+          run_left_and_right.call(left, right, rest) do
+            assemblies << "  cmp rax, rdi"
+            assemblies << "  setne al"
+            assemblies << "  movzb rax, al"
+          end
+        in [Node::Lt[left, right], *rest]
+          run_left_and_right.call(left, right, rest) do
+            assemblies << "  cmp rax, rdi"
+            assemblies << "  setl al"
+            assemblies << "  movzb rax, al"
+          end
+        in [Node::Lte[left, right], *rest]
+          run_left_and_right.call(left, right, rest) do
+            assemblies << "  cmp rax, rdi"
+            assemblies << "  setle al"
+            assemblies << "  movzb rax, al"
+          end
         in [Node::Add[left, right], *rest]
-          run(left, assemblies)
-          run(right, assemblies)
-          assemblies << "  pop rdi"
-          assemblies << "  pop rax"
-          assemblies << "  add rax, rdi"
-          run(rest, assemblies)
+          run_left_and_right.call(left, right, rest) do
+            assemblies << "  add rax, rdi"
+          end
         in [Node::Sub[left, right], *rest]
-          run(left, assemblies)
-          run(right, assemblies)
-          assemblies << "  pop rdi"
-          assemblies << "  pop rax"
-          assemblies << "  sub rax, rdi"
-          run(rest, assemblies)
+          run_left_and_right.call(left, right, rest) do
+            assemblies << "  sub rax, rdi"
+          end
         in [Node::Mul[left, right], *rest]
-          run(left, assemblies)
-          run(right, assemblies)
-          assemblies << "  pop rdi"
-          assemblies << "  pop rax"
-          assemblies << "  imul rax, rdi"
-          run(rest, assemblies)
+          run_left_and_right.call(left, right, rest) do
+            assemblies << "  imul rax, rdi"
+          end
         in [Node::Div[left, right], *rest]
-          run(left, assemblies)
-          run(right, assemblies)
-          assemblies << "  pop rdi"
-          assemblies << "  pop rax"
-          assemblies << "  cqo"
-          assemblies << "  idiv rdi"
-          run(rest, assemblies)
+          run_left_and_right.call(left, right, rest) do
+            assemblies << "  cqo"
+            assemblies << "  idiv rdi"
+          end
         end
       end
 
@@ -89,8 +109,8 @@ class CLI
   end
 
   def inputs
-    args = parser.parse(@argv)
-    expression = args.shift
+    expression = @argv.shift
+    parser.parse(@argv)
 
     show_usage if expression.empty?
 
@@ -119,7 +139,7 @@ class CLI
     def show_usage(error = nil)
       puts "error: #{error.message}\n" if error
       puts <<~EOF
-      #{parser.to_s}
+        #{parser.to_s}
       EOF
       exit 1
     end
