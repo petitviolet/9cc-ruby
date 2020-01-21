58e4e51fa491a687d00faedc1c362b65f0b802f6
petitviolet <violethero0820@gmail.com>
2020-01-21 22:44:38 +0900

    Use Generator as an instance not static function

--- lib/9cc/generator.rb
+++ lib/9cc/generator.rb
@@ -2,69 +2,77 @@ require 'rstructural'
 require_relative './node'
 
 class Generator
-  def self.run(nodes, assemblies = [])
+  def self.run(nodes, outputs)
+    new(outputs).run(nodes)
+  end
+
+  def initialize(outputs)
+    @outputs = outputs
+  end
+
+  def run(nodes)
     case Array(nodes).flatten
     in []
       return []
     in [Node::Num[value], *rest]
-      assemblies << "  push #{value}"
-      return run(rest, assemblies)
+      @outputs << "  push #{value}"
+      return run(rest)
     else
       run_left_and_right = ->(left, right, rest, &block) do
-        run(left, assemblies)
-        run(right, assemblies)
-        assemblies << "  pop rdi"
-        assemblies << "  pop rax"
+        run(left)
+        run(right)
+        @outputs << "  pop rdi"
+        @outputs << "  pop rax"
         block.call
-        run(rest, assemblies)
+        run(rest)
       end
 
       case Array(nodes).flatten
       in [Node::Eq[left, right], *rest]
         run_left_and_right.call(left, right, rest) do
-          assemblies << "  cmp rax, rdi"
-          assemblies << "  sete al"
-          assemblies << "  movzb rax, al"
+          @outputs << "  cmp rax, rdi"
+          @outputs << "  sete al"
+          @outputs << "  movzb rax, al"
         end
       in [Node::Neq[left, right], *rest]
         run_left_and_right.call(left, right, rest) do
-          assemblies << "  cmp rax, rdi"
-          assemblies << "  setne al"
-          assemblies << "  movzb rax, al"
+          @outputs << "  cmp rax, rdi"
+          @outputs << "  setne al"
+          @outputs << "  movzb rax, al"
         end
       in [Node::Lt[left, right], *rest]
         run_left_and_right.call(left, right, rest) do
-          assemblies << "  cmp rax, rdi"
-          assemblies << "  setl al"
-          assemblies << "  movzb rax, al"
+          @outputs << "  cmp rax, rdi"
+          @outputs << "  setl al"
+          @outputs << "  movzb rax, al"
         end
       in [Node::Lte[left, right], *rest]
         run_left_and_right.call(left, right, rest) do
-          assemblies << "  cmp rax, rdi"
-          assemblies << "  setle al"
-          assemblies << "  movzb rax, al"
+          @outputs << "  cmp rax, rdi"
+          @outputs << "  setle al"
+          @outputs << "  movzb rax, al"
         end
       in [Node::Add[left, right], *rest]
         run_left_and_right.call(left, right, rest) do
-          assemblies << "  add rax, rdi"
+          @outputs << "  add rax, rdi"
         end
       in [Node::Sub[left, right], *rest]
         run_left_and_right.call(left, right, rest) do
-          assemblies << "  sub rax, rdi"
+          @outputs << "  sub rax, rdi"
         end
       in [Node::Mul[left, right], *rest]
         run_left_and_right.call(left, right, rest) do
-          assemblies << "  imul rax, rdi"
+          @outputs << "  imul rax, rdi"
         end
       in [Node::Div[left, right], *rest]
         run_left_and_right.call(left, right, rest) do
-          assemblies << "  cqo"
-          assemblies << "  idiv rdi"
+          @outputs << "  cqo"
+          @outputs << "  idiv rdi"
         end
       end
     end
 
-    assemblies << "  push rax"
+    @outputs << "  push rax"
   end
 end
 
