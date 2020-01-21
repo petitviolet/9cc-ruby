a6cbfdd6d25581aebfcc6392b8ccfcf68738a81a
petitviolet <violethero0820@gmail.com>
2020-01-22 00:00:34 +0900

    Enabled to add -v option to test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 try() {
   expected="$1"
   input="$2"
+  option="$3"
 
-  rbenv exec ruby -W0 lib/9cc.rb "$input" > tmp.s
+  rbenv exec ruby -W0 lib/9cc.rb "$input" $option > tmp.s
   # ./9cc "$input" > tmp.s
   gcc -o tmp tmp.s
   ./tmp
@@ -31,8 +33,10 @@ run_test() {
   logging "start"
   try 0 0
   try 42 42
+  try 2 '0 + 2'
   try 47 '5+6*7'
   try 15 '5*(9-6)'
+  try 15 '5 * ( 9 - 6 )'
   try 4 '(3+5)/2'
   try 23 '(1 + (2 + 3) * (4 + 5)) / 2'
   try 10 '-10+20'
@@ -46,13 +50,12 @@ run_test() {
   try 0 '2 != 2'
   try 0 '1 == 2'
   try 1 '2 == 2'
-  try 5 'a = 2; b = 3; a + b'
   logging "OK"
 }
 
 if [ $# -eq 0 ]; then
   run_test
 else
-  try $1 $2
+  try "$@"
 fi
 
